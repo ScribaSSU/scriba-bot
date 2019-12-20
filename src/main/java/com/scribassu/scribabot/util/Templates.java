4856595a2ff702fc3095b88f992fa596458a9d8a
sveta.celuykina <sveta.celuykina@mail.ru>
2019-12-20 13:18:44 +0400

    fixed lessonType, weekType, added subGroup

--- src/main/java/com/scribassu/scribabot/util/Templates.java
+++ src/main/java/com/scribassu/scribabot/util/Templates.java
@@ -2,31 +2,28 @@ package com.scribassu.scribabot.util;
 
 import com.scribassu.tracto.domain.FullTimeLesson;
 import com.scribassu.tracto.domain.LessonType;
-import com.scribassu.tracto.domain.WeekType;
 
 import java.util.List;
 
 public class Templates {
     public String makeTemplate(List<FullTimeLesson> fullTimeLessons)
     {
-        fullTimeLessons.sort((o1, o2) -> (int) (o1.getLessonTime().getId() - o2.getLessonTime().getId()));
+        fullTimeLessons.sort((o1, o2) -> (int) (o1.getLessonTime().getLessonNumber() - o2.getLessonTime().getLessonNumber()));
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Сегодня ").append(fullTimeLessons.get(0).getDay().getWeekDay().getDay()).append("\n Неделя - ");
-        if (fullTimeLessons.get(0).getWeekType().equals(WeekType.NOM))
-            stringBuilder.append("числитель ").append("\uD83D\uDD4A \n");
-        else if (fullTimeLessons.get(0).getWeekType().equals(WeekType.DENOM))
-            stringBuilder.append("знаменатель ").append("\uD83D\uDD4A \n");
+        stringBuilder.append(fullTimeLessons.get(0).getWeekType().getType()).append("\uD83D\uDD4A \n");
         stringBuilder.append("Группа № ").append(fullTimeLessons.get(0).getStudentGroup().getGroupNumber()).append("\n");
         for (FullTimeLesson fullTimeLesson : fullTimeLessons) {
             stringBuilder.append(fullTimeLesson.getLessonTime().getTimeStart()).append(" - ")
-                   .append(fullTimeLesson.getLessonTime().getTimeFinish()).append("\n");
+                   .append(fullTimeLesson.getLessonTime().getTimeFinish()).append("\n")
+                   .append(fullTimeLesson.getLessonType().getType());
             if (fullTimeLesson.getLessonType().equals(LessonType.LECTURE))
-                stringBuilder.append("Лекция \uD83D\uDCD7 \n");
+                stringBuilder.append(" \uD83D\uDCD7 \n");
             else if (fullTimeLesson.getLessonType().equals(LessonType.PRACTICE))
-                 stringBuilder.append("Практика \uD83D\uDCD8 \n");
+                 stringBuilder.append(" \uD83D\uDCD8 \n");
             else if (fullTimeLesson.getLessonType().equals(LessonType.LABORATORY))
-                 stringBuilder.append("Лабораторная \uD83D\uDCD5 \n");
-            stringBuilder.append(fullTimeLesson.getName()).append("\n")
+                 stringBuilder.append(" \uD83D\uDCD5 \n");
+            stringBuilder.append(fullTimeLesson.getSubGroup()).append("\n").append(fullTimeLesson.getName()).append("\n")
                     .append(fullTimeLesson.getTeacher().getSurname()).append(" ")
                     .append(fullTimeLesson.getTeacher().getName()).append(" ")
                     .append(fullTimeLesson.getTeacher().getPatronymic()).append("\n")
