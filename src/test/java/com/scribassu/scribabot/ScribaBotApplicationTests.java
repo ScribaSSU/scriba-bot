326c3f3f50bc2f9df416f4dc84134de38eacdc05
Dzeru <diana.mashkina.00@gmail.com>
2021-06-30 20:24:42 +0400

    update framework and libraries versions

--- src/test/java/com/scribassu/scribabot/ScribaBotApplicationTests.java
+++ src/test/java/com/scribassu/scribabot/ScribaBotApplicationTests.java
@@ -1,13 +1,10 @@
 package com.scribassu.scribabot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @ActiveProfiles(profiles = {"dev"})
 public class ScribaBotApplicationTests {
 
